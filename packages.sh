d9e643a4737c844bd24ba979112a102c50969ce2
mahabub96 <137638133+mahabub96@users.noreply.github.com>
2025-08-17 02:43:38 +0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,3 +1,9 @@
 pip uninstall opencv-python opencv-contrib-python opencv-python-headless -y
 pip cache purge
 pip install opencv-python-headless==4.5.5.64
+
+#!/bin/bash
+echo "deb http://deb.debian.org/debian bullseye main contrib non-free" > /etc/apt/sources.list.d/opencv.list
+echo "deb http://deb.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list.d/opencv.list
+apt-get update
+apt-get install -y libopencv-dev
